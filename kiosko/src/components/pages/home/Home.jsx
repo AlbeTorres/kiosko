1dbd04567ea2d4286da613539f9485795c1c3a06
albetorres <albertocorreoficial@gmail.com>
2022-10-21 13:45:29 -0300

    eso

--- kiosko/src/components/pages/home/Home.jsx
+++ kiosko/src/components/pages/home/Home.jsx
@@ -3,11 +3,10 @@ import HomeContainer from "../../layout/HomeContainer";
 import Tablero from "../../layout/Tablero";
 import Ubicacion from '../../layout/Ubicacion'
 import Navbar from '../../layout/Navbar'
-import Hero from '../../layout/Hero'
 import Footer from '../../layout/Footer'
 import Modal from '../../layout/Modal'
 import productoContext from "../../../context/productoContext/productoContext";
-import BusquedaList from "../../busqueda/BusquedaList";
+
 
 const Home = () => {
 
