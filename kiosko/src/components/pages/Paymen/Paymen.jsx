f3b412d132738217d6efc187abaf01479eba1709
albetorres <albertocorreoficial@gmail.com>
2022-10-22 19:32:56 -0300

    eso

--- kiosko/src/components/pages/Paymen/Paymen.jsx
+++ kiosko/src/components/pages/Paymen/Paymen.jsx
@@ -5,7 +5,7 @@ import cup from "../../../assets/cup.jpg";
 const Paymen = () => {
   return (
     <div >
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 md:px-4 px-2">
         <div className="flex-1">
           <a className="btn btn-ghost normal-case text-xl">Entrega</a>
         </div>
