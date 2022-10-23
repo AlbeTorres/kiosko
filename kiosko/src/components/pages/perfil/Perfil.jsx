ef2c587c0b141741ef5ad97d8012aadc1797ee8c
albetorres <albertocorreoficial@gmail.com>
2022-10-23 16:39:24 -0300

    terminado perfil vista responsive

--- kiosko/src/components/pages/perfil/Perfil.jsx
+++ kiosko/src/components/pages/perfil/Perfil.jsx
@@ -7,26 +7,32 @@ import {
   FaPhoneAlt,
   FaUserLock,
 } from "react-icons/fa";
+import img2 from '../../../assets/1.jpg'
 
 const Perfil = () => {
   return (
     <div>
-      <div className="bg-primary h-52 w-full grid place-items-center text-white ">
-        <div className="flex items-center justify-between px-4 w-full ">
-          <button>atras</button>
-          <button>menu</button>
+      <div className=" h-52 w-full grid place-items-center text-black  rounded-b-lg  md:flex md:rounded-none md:h-20 md:px-4  ">
+        <div className="flex items-center justify-between md:justify-start px-4 w-full ">
+          <button className="mr-4">Volver</button>
+          <button>Editar</button>
         </div>
+        <div className="grid place-items-center md:flex md:justify-center md:items-center md:flex-row-reverse">
         <div className="avatar">
-          <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+          <div className="w-24 md:w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
             <img src="https://placeimg.com/192/192/people" />
           </div>
         </div>
-        <div className="mb-2">
-          <p>Nombre</p>
+        <div className="my-2 md:w-32 ">
+          <p>Geustakio Perez</p>
+        </div>
+
         </div>
       </div>
 
-      <div className="grid w-11/12 mx-auto place-items-start ">
+      <div className="grid md:grid-cols-2 md:gap-x-8 md:mt-8  md:h-full w-11/12 mx-auto place-items-start ">
+        <div className="md:w-full">
+
         <div className="border-b w-full my-4     ">
           <div className="flex items-center ">
             <FaEnvelope />
@@ -55,13 +61,17 @@ const Perfil = () => {
           </div>
           <p className="ml-5">Artemisa</p>
         </div>
+        </div>
         
-        <div className='border-b w-full my-4'>
+        <div className=' w-full my-4'>
           <div className="flex items-center ">
             <FaUserLock />
             <p className="ml-1">Kyc</p>
           </div>
-          <img src="" alt="" />
+          <figure className="w-3/4 mx-auto rounded-md shadow-md">
+          <img className="w-full object-cover rounded-md" src={img2} alt="" />
+
+          </figure>
         </div>
       </div>
     </div>
