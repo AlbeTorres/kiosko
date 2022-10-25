f392198746d5d95e86f19cbca376eb3c71acaffe
albetorres <albertocorreoficial@gmail.com>
2022-10-25 14:36:11 -0300

    modificada vista crear pedido

--- kiosko/src/components/Payment/PayForm.jsx
+++ kiosko/src/components/Payment/PayForm.jsx
@@ -1,46 +1,84 @@
-import React from 'react'
+import React from "react";
 
 const PayForm = () => {
   return (
-    <div className="md:relative">
-    <h2 className="text-center text-xl font-semibold my-5 ">
-      Formulario de entrega
-    </h2>
+    <div className="md:relative  ">
+      <h2 className="text-center text-xl font-semibold my-5 ">
+        Formulario de entrega
+      </h2>
 
-    <form className="grid place-content-center ">
-      <div className="form-control ">
-        <label className="label">
-          <span className="label-text">Dirección de entrega</span>
-        </label>
-        <label className="input-group">
-          <span>Dirección</span>
-          <input
-            type="text"
-            placeholder="info@site.com"
-            className="input input-bordered"
-          />
-        </label>
-      </div>
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Contacto del receptor</span>
-        </label>
-        <label className="input-group">
-          <span>Contácto</span>
-          <input
-            type="text"
-            placeholder="+53xxxxxxxx"
-            className="input input-bordered"
-          />
-        </label>
-      </div>
-    </form>
-    <div className="w-11/12 mx-auto mt-5 md:w-52  md:flex md:items-center md: justify-start md:absolute md:bottom-32  md:left-28  ">
-        <button className="btn btn-primary w-full my-2 md:mr-2">Crear pedido</button>
-        <button className="btn w-full mb-2 md:m-0">Cancelar</button>
-      </div>
-  </div>
-  )
-}
+      <form className="grid place-items-center ">
+        <div className="form-control mt-5 ">
+          <label className="label">
+            <span className="label-text">Dirección de entrega</span>
+          </label>
+          <label className="input-group">
+            <span>Dirección</span>
+            <input
+              type="text"
+              placeholder="info@site.com"
+              className="input input-bordered"
+            />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Nombre del receptor</span>
+          </label>
+          <label className="input-group">
+            <span>Nombre</span>
+            <input
+              type="text"
+              placeholder="Nombre"
+              className="input input-bordered"
+            />
+          </label>
+        </div>
+        <div className="form-control mb-5">
+          <label className="label">
+            <span className="label-text">Contacto del receptor</span>
+          </label>
+          <label className="input-group">
+            <span>Contácto</span>
+            <input
+              type="text"
+              placeholder="+53xxxxxxxx"
+              className="input input-bordered"
+            />
+          </label>
+        </div>
+        <h2 className="text-center text-xl font-semibold  ">
+        Método de pago
+      </h2>
+        <div className="form-control my-5">
+          <label className=" flex items-center cursor-pointer   w-24 ">
+            <input
+              type="radio"
+              name="radio-6"
+              className="radio checked:bg-red-500 mr-2"
+              checked
+            />
+            <span className="label-text">Efectivo</span>
+          </label>
+        </div>
+        <div className="form-control mb-5 ">
+          <label className="flex items-center cursor-pointer   w-24 ">
+            <input
+              type="radio"
+              name="radio-6"
+              className="radio checked:bg-blue-500 mr-2"
+              checked
+            />
+            <span className="label-text ">Transfermóvil</span>
+          </label>
+        </div>
+        <div className=" mt-5 grid grid-cols-2 place-items-center gap-2   ">
+          <button className="btn w-full ">Cancelar</button>
+          <button className="btn btn-primary w-full ">Crear pedido</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default PayForm
+export default PayForm;
