d6553e8a4a30373453e2d46d8a14e1933df2d63a
albetorres <albertocorreoficial@gmail.com>
2022-11-11 17:07:56 -0500

    terminado lista linda de productos en pedido

--- kiosko/src/components/pedidos/PedidoItemProductos.jsx
+++ kiosko/src/components/pedidos/PedidoItemProductos.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 
 const PedidoItemProductos = ({ productos }) => {
+
+  let count = 0;
+  let more=false
   return (
     <div className="avatar-group -space-x-6">
       {productos.map((producto, index) => {
-        return(
 
-        <div className="avatar">
-          <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src={producto.img} />
-          </div>
-        </div>
-        )
+          if(count < 5){
+            count= count +1
+            return(
+
+            <div key={index} className="avatar">
+              <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img src={producto.img} />
+              </div>
+            </div>
+            )
+
+          }else{
+            more=true
+            count= productos.length -5
+          }
       }
       )}
+
+      {more && 
       <div className="avatar placeholder">
         <div className="w-12 bg-neutral-focus text-neutral-content">
-          <span>+99</span>
+          <span>{`+${count}`}</span>
         </div>
-      </div>
+      </div>}
     </div>
   );
 };
