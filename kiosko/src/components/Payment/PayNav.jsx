6e8479d7a1a5af0de3de1b7528a112dd564e7a00
albetorres <albertocorreoficial@gmail.com>
2022-11-23 09:15:14 -0500

    eliminado mapa en paymen navbar

--- kiosko/src/components/Payment/PayNav.jsx
+++ kiosko/src/components/Payment/PayNav.jsx
@@ -6,13 +6,7 @@ const PayNav = () => {
     <div className="flex-1">
       <a className="btn btn-ghost normal-case text-xl">Entrega</a>
     </div>
-    <div className="flex-none">
-      <ul className="menu menu-horizontal p-0 ">
-        <li>
-          <a>Mapa</a>
-        </li>
-      </ul>
-    </div>
+    
   </div>
   )
 }
