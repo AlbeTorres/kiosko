24b1c49b1ebd2c53923c762b4015429253508907
albetorres <albertocorreoficial@gmail.com>
2022-11-24 12:23:01 -0500

    usuario admin

--- kiosko/src/pages/Admin/adminuser/AdminUser.jsx
+++ kiosko/src/pages/Admin/adminuser/AdminUser.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {FaUserPlus,FaArrowLeft} from 'react-icons/fa'
+import Adminusercard from './Adminusercard'
 
 const AdminUser = () => {
   return (
     <div >
         <div className='navbar bg-primary text-primary-content'>
-        <div className='flex w-full items-center justify-between px-2'>
+        <div className='flex w-full md:w-11/12 mx-auto items-center justify-between px-2'>
         <div className='flex items-center '>
             <button><FaArrowLeft className='text-xl '/></button>
             <p className='ml-2 hidden md:block' >atrás</p>
@@ -19,8 +20,24 @@ const AdminUser = () => {
 
         </div>
         </div>
-        <div className='border-2 border-gray-200 rounded-b-md overflow-y-auto '>
-           
+        <div className=' px-2 md:w-3/4 md:mx-auto'>
+
+        <div className='  overflow-y-auto md:overflow-hidden md:hover:overflow-auto '>
+           <h2 className='w-full text-center rounded-t-md text-lg font-bold bg-primary text-white mt-5 p-2'>
+            Usuarios
+           </h2>
+
+           <Adminusercard/>
+           <Adminusercard/>
+           <Adminusercard/>
+           <Adminusercard/>
+           <Adminusercard/>
+           <Adminusercard/>
+           <Adminusercard/>
+           <Adminusercard/>
+           <Adminusercard/>
+
+        </div>
         </div>
     </div>
   )
--- kiosko/src/pages/Admin/adminuser/Adminusercard.jsx
+++ kiosko/src/pages/Admin/adminuser/Adminusercard.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
+import React, {useState} from "react";
+import img from "../../../assets/noimg1.jpg";
+import { FaCheckCircle } from "react-icons/fa";
+import { FaHamburger } from "react-icons/fa";
 
 const Adminusercard = () => {
+
+    const [visible, setVisible]=useState(false)
+    let baneado= true
   return (
-    <div className='usuario'>
-    <div className='img'>
+    <div className="flex items-center justify-between gap-2 border-b-2 border-gray-200  rounded-b-md ">
+      <figure className="md:w-16  md:h-16 w-14 h-14 rounded-full m-1 shadow-md relative  ">
+        <FaCheckCircle className="text-blue-700 rounded-full bg-white text-lg  absolute bottom-1 right-2 "/>
+        <img className="w-full h-full object-cover rounded-full" src={img} />
+      </figure>
+      <div className="flex-auto ml-5 md:flex md:items-center ">
+      <div>
+        <p className={baneado ? 'text-red-700': 'text-black'}>Alberto Torres Reyes</p>
+        <p >Administrador</p>
+      </div>
+      <div className="md:items-center  md:ml-6  hidden md:flex  ">
+        <p className="mr-2">CI:</p>
+        <p className={baneado ? 'text-red-700': 'text-black'}>96032204741</p>
+      </div>
+      </div>
 
-    </div>
-    <div className='datos'>
-        <p>nombre</p>
-        <p>rol</p>
-    </div>
-    <div>
-        <p>
-        estado del kyc
-        </p>  
-    </div>
-    <div>
-        baneado
-    </div>
 
-    <div>
-        <button>targeta roja</button>
-        <button>activar kyc</button>
-        <button>modificar</button>
-        <button>eliminar</button>
+      <div className="mr-2 md:hidden">
+            <FaHamburger onClick={()=>setVisible(!visible)} className='text-lg'/>
+            {visible && 
+            <ul className="mt-3 p-2 right-2 absolute shadow bg-base-100 rounded-md grid grid-flow-row gap-2">
+                    <button className="btn btn-ghost btn-xs">targeta roja</button>
+                    <button className="btn btn-ghost btn-xs">activar kyc</button>
+                    <button className="btn btn-ghost btn-xs">modificar</button>
+                    <button className="btn btn-ghost btn-xs">eliminar</button>
+            </ul>}
+        </div> 
 
+        <div className=" hidden md:grid md:grid-cols-4 ">
+        <button className="btn btn-ghost btn-xs">targeta roja</button>
+                    <button className="btn btn-ghost btn-xs">activar kyc</button>
+                    <button className="btn btn-ghost btn-xs">modificar</button>
+                    <button className="btn btn-ghost btn-xs">eliminar</button>
+        </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default Adminusercard
+export default Adminusercard;
