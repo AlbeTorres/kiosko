2cee0e336e7dda99de4a0bdfa3ac62f4055f0173
albetorres <albertocorreoficial@gmail.com>
2022-12-05 15:36:37 -0500

    pedidoState mal escrito solved

--- kiosko/src/App.jsx
+++ kiosko/src/App.jsx
@@ -3,7 +3,7 @@ import Rutas from "./routes/Rutas";
 import ProductoState from "./context/productoContext/productoState";
 import AuthState from "./context/authContext/authState";
 import AlertaState from "./context/alertaContext/alertaState";
-import PedidoState from "./context/pedidoContext/PedidoState.jsx";
+import PedidoState from "./context/pedidoContext/pedidoState.jsx";
 import tokenAuth from "./config/tokenAuth";
 import "./index.css";
 import UsuarioState from "./context/usuarioContext/usuarioState";
