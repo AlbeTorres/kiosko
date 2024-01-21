a5230d1fdba19e50bf07853b7de527a23eee6a87
AlbeTorres <albertocorreoficial@gmail.com>
2024-01-21 11:52:39 -0500

    feat: add new logo

--- kiosko/src/layouts/Logo.tsx
+++ kiosko/src/layouts/Logo.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { FaShoppingCart } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.webp'
 
-export const Logo = () => ( <div className='flex items-center'>
-<FaShoppingCart className='text-xl' />
-<Link to={'/'} className='normal-case text-xl ml-1 '>
-  Kiosquito
-</Link>
-</div>)
+export const Logo = () => (
+  <div className='flex items-center'>
+    {/* <FaShoppingCart className='text-xl' /> */}
+    <img src={logo} className='w-10 h-10' />
+    <Link to={'/'} className='normal-case text-xl ml-1 '>
+      Kiosquito
+    </Link>
+  </div>
+)
